3546b4c5e1c9493e07089ddedd7bf85f0e5698fb
KOTOFEYS <makarov.k@mail.ru>
2024-09-14 23:00:43 +0300

    Домашнее задание по теме "Оператор "with".

--- modul7.1/modul7.3.py
+++ modul7.1/modul7.3.py
@@ -9,38 +9,53 @@ class WordsFinder:
 
     def get_all_words(self):
         all_words = {}
+        words = []
         for name in self.file_name:
             with open (name, encoding = 'utf - 8') as file:
-                file.read()
                 for i in file:
                     i = i.lower()
+                    string.punctuation
                     for p in string.punctuation:
-                        if p in file:
-                            file = file.replace(p,'-').split()
-        all_words[self.file_name] = file
+                        if i == '`':
+                            i = i.replace(p,' ')
+                    words.extend(i.split())
+        all_words [self.file_name] = words
         return all_words
 
     def find(self,word):
         word = word.lower()
         other_words = {}
-        list_words = []
+        # list_words = []
         for name, words in self.get_all_words().items():
-            list_words.append(words)
             if word in words:
-                other_words[name] = list_words.index(word)
+                other_words[name] = words.index(word)+1
                 return other_words
 
     def count(self,word):
         word = word.lower()
         other_words = {}
-        list_words = []
         for name, words in self.get_all_words().items():
-            list_words.append(words)
+            # list_words.append(words)
             if word in words:
-                other_words[name] = list_words.len(word)
+                other_words[name] = len(word)
                 return other_words
 
 finder2 = WordsFinder('test_file.txt')
 print(finder2.get_all_words()) # Все слова
 print(finder2.find('TEXT')) # 3 слово по счёту
 print(finder2.count('teXT')) # 4 слова teXT в тексте всего
+# # os.getcwd()
+
+
+# line = '- Мама мыла раму, а Маша помогала. (из прописи 1 класса)'
+# import string
+# string.punctuation
+# # '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+#
+# # пунктуацию будем удалять в цикле
+# for p in string.punctuation:
+#     if p in line:
+#         # банальная замена символа в строке
+#         line = line.replace(p, '')
+#
+# line.strip()
